52e561be378bc8f43660320aaccf4859695368d7
Ivan_Olguin <34221558+ivanalejandro2002@users.noreply.github.com>
2025-04-13 23:59:27 -0600

    Agregando problemas

---
+++ UvaJudge/10000/10400/10466howFar.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fi first
+#define se second
+#define pb push_back
+#define LSOne(S) ((S)&(-S))
+#define all(S) S.begin(),S.end()
+#define fore(V,I,F) for(int V=I;V<=F;V++)
+typedef long long int ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
+typedef vector<vi> vvi;
+typedef vector<vll> vvll;
+typedef pair<int,int> pii;
+typedef vector<pii> vpii;
+typedef vector<vpii> vvpii;
+typedef long double ld;
+
+const ld eps = 1e-9, inf = numeric_limits<ld>::max(), pi = acos(-1);
+// For use with integers, just set eps=0 and everything remains the same
+bool geq(ld a, ld b){return a-b >= -eps;}     //a >= b
+bool leq(ld a, ld b){return b-a >= -eps;}     //a <= b
+bool ge(ld a, ld b){return a-b > eps;}        //a > b
+bool le(ld a, ld b){return b-a > eps;}        //a < b
+bool eq(ld a, ld b){return abs(a-b) <= eps;}  //a == b
+bool neq(ld a, ld b){return abs(a-b) > eps;}  //a != b
+
+struct point{
+	ld x, y;
+	point(): x(0), y(0){}
+	point(ld x, ld y): x(x), y(y){}
+
+	point operator+(const point & p) const{return point(x + p.x, y + p.y);}
+	point operator-(const point & p) const{return point(x - p.x, y - p.y);}
+	point operator*(const ld & k) const{return point(x * k, y * k);}
+	point operator/(const ld & k) const{return point(x / k, y / k);}
+
+	point operator+=(const point & p){*this = *this + p; return *this;}
+	point operator-=(const point & p){*this = *this - p; return *this;}
+	point operator*=(const ld & p){*this = *this * p; return *this;}
+	point operator/=(const ld & p){*this = *this / p; return *this;}
+
+	point rotate(const ld & a) const{return point(x*cos(a) - y*sin(a), x*sin(a) + y*cos(a));}
+	point perp() const{return point(-y, x);}
+	ld ang() const{
+		ld a = atan2l(y, x); a += le(a, 0) ? 2*pi : 0; return a;
+	}
+	ld dot(const point & p) const{return x * p.x + y * p.y;}
+	ld cross(const point & p) const{return x * p.y - y * p.x;}
+	ld norm() const{return x * x + y * y;}
+	ld length() const{return sqrtl(x * x + y * y);}
+	point unit() const{return (*this) / length();}
+
+	bool operator==(const point & p) const{return eq(x, p.x) && eq(y, p.y);}
+	bool operator!=(const point & p) const{return !(*this == p);}
+	bool operator<(const point & p) const{return le(x, p.x) || (eq(x, p.x) && le(y, p.y));}
+	bool operator>(const point & p) const{return ge(x, p.x) || (eq(x, p.x) && ge(y, p.y));}
+	bool half(const point & p) const{return le(p.cross(*this), 0) || (eq(p.cross(*this), 0) && le(p.dot(*this), 0));}
+};
+
+void solve(int n, ld t){
+    vector<point> planets(n);
+    ld cicles;
+    fore(i,0,n-1){
+        cin>>planets[i].x;
+        planets[i].y = 0;
+
+        cin>>cicles;
+        ld angle = (t/cicles)*2*pi;
+
+        planets[i] = planets[i].rotate(angle);
+    }
+    point acumulado(0,0);
+    fore(i,0,n-1){
+        
+
+        if(i>0)cout<<" ";
+        cout<<fixed<<setprecision(4)<<(acumulado + planets[i]).length();
+        acumulado= acumulado + planets[i];
+    }
+    cout<<"\n";
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int n;
+    ld t;
+    while(cin>>n){
+        cin>>t;
+        solve(n,t);
+    }
+}
